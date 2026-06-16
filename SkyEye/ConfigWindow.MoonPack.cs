65be1fe279fe0cf5c894d8680d067a4cf7781c6d
Latihas <lyjp2002@163.com>
2026-06-16 16:42:58 +0800

    7.51.1.1

--- SkyEye/ConfigWindow.MoonPack.cs
+++ SkyEye/ConfigWindow.MoonPack.cs
@@ -1,16 +1,33 @@
 ﻿using Dalamud.Bindings.ImGui;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace SkyEye;
 
 public partial class ConfigWindow {
 	private static unsafe void DrawMoonPack() {
-		if (Plugin.MoonPackId == 0) return;
-		if (!isFindingMoonPack && ImGui.Button("开始自动寻宝")) {
-			isFindingMoonPack = true;
-			Plugin.Framework.RunOnTick(() => AgentInventoryContext.Instance()->UseItem(Plugin.MoonPackId));
+		try {
+			ImGui.Text("CE进度");
+			var addon = Plugin.Gui.GetAddonByName<AtkUnitBase>("WKSAnnounce");
+			var uld = addon->UldManager;
+			for (var i = 0; i < uld.NodeListSize; i++) {
+				var node = uld.NodeList[i];
+				if (!node->IsVisible() ||(int)node->Type != 1002) continue;
+				var comp = ((AtkComponentNode*)node)->Component;
+				if (comp->GetComponentType() != ComponentType.GaugeBar) continue;
+				var jg = (AtkComponentGaugeBar*)comp;
+				ImGui.Text($"{jg->Values[0].ValueFloat}/{jg->MaxValue}");
+			}
+		} catch {
+			//
 		}
-		if (isFindingMoonPack && ImGui.Button("停止自动寻宝"))
-			isFindingMoonPack = false;
+		if (Plugin.MoonPackId != 0) {
+			if (!isFindingMoonPack && ImGui.Button("开始自动寻宝")) {
+				isFindingMoonPack = true;
+				Plugin.Framework.RunOnTick(() => AgentInventoryContext.Instance()->UseItem(Plugin.MoonPackId));
+			}
+			if (isFindingMoonPack && ImGui.Button("停止自动寻宝"))
+				isFindingMoonPack = false;
+		}
 	}
 }
